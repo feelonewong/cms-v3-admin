bdf3a996d591cde5fd8dbc76e0fb9474f300fdbd
feelonewong <feelonewong@gmail.com>
2023-06-10 19:54:54 +0800

    update:13配置请求代理

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,15 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
+// import request from '@/utils/request'
 onMounted(() => {
   console.log('App mounted!')
+  // request({
+  //   url: '/ad/getAdList',
+  //   method: 'GET'
+  // }).then((res: Object) => {
+  //   console.log(res)
+  // })
 })
 </script>
 
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const request = axios.create({
-  // baseURL: import.meta.env.VITE_BASE_URL as string
-  baseURL: '/front/ad/getAdList'
+  baseURL: import.meta.env.VITE_BASE_URL as string
 })
 export default request
