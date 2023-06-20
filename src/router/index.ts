42785123d5a95e04800bed52aef06d5bd9d9e672
feelonewong <feelonewong@gmail.com>
2023-06-20 23:43:56 +0800

    update: 49资源列表搜索

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppLayout from '@/components/layout/AppLayout.vue'
 import { useTokenStore } from '@/stores/token'
+// @ts-ignore
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
--- src/views/resources/resourceIndex.vue
+++ src/views/resources/resourceIndex.vue
@@ -5,10 +5,43 @@
         <h3>资源类别列表</h3>
       </div>
     </template>
+    <el-form :model="queryParams" ref="formRef" label-width="120px" :label-position="'right'">
+      <el-row type="flex" gutter="10">
+        <el-col :span="6">
+          <el-form-item label="资源名称" prop="name">
+            <el-input v-model="queryParams.name" placeholder="请输入资源名称" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-form-item label="资源路径" prop="url">
+            <el-input v-model="queryParams.url" placeholder="请输入资源路径" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-form-item label="资源分类" prop="categoryId">
+            <el-select v-model="queryParams.categoryId" placeholder="请选择资源分类">
+              <el-option label="不限" value="" />
+              <el-option
+                v-for="item in resourceType"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6" style="padding-left: 20px">
+          <el-row type="flex" justify="start">
+            <el-button type="info" @click="handleReset">重置</el-button>
+            <el-button type="primary" @click="handleSearch">搜索</el-button>
+          </el-row>
+        </el-col>
+      </el-row>
+    </el-form>
     <el-row>
       <el-col>
         <el-button type="primary">添加资源</el-button>
-        <el-button type="primary">资源类别</el-button>
+        <el-button type="primary" @click="handleJumpCategory">资源类别</el-button>
       </el-col>
     </el-row>
     <el-table :data="tableData" height="580" border style="width: 100%" v-loading="tableLoading">
@@ -45,12 +78,18 @@
 <script setup lang="ts">
 import { onMounted, reactive, ref } from 'vue'
 import { getResourceList } from '@/api/resources.ts'
+import { getAllResource } from '@/api/resource-category'
+
 import type { Condition } from '@/api/resources.ts'
+import { useRouter } from 'vue-router'
 import ElMessage from 'element-plus/lib/components/message/index.js'
 import dayjs from 'dayjs'
+import { FormInstance } from 'element-plus/lib/components'
 onMounted(() => {
+  getResourceType() // 获取资源分类List
   queryList()
 })
+const router = useRouter()
 let tableData = reactive([])
 let total = ref(0)
 
@@ -61,7 +100,30 @@ let queryParams = reactive<Condition>({
   current: 1,
   size: 20
 })
-
+const formRef = ref<FormInstance>()
+const handleReset = () => {
+  formRef.value?.resetFields()
+}
+const handleSearch = () => {
+  queryParams.current = 1
+  queryList()
+}
+const resourceType = ref([])
+const getResourceType = () => {
+  resourceType.value = []
+  getAllResource()
+    .then((res) => {
+      if (res.data.code === '000000') {
+        resourceType.value = res.data.data
+      } else {
+        ElMessage.error('获取资源类别失败')
+        throw new Error('获取资源类别失败')
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 const tableConfig = reactive([
   { label: '序号', width: '80', align: 'center', prop: '', type: 'index' },
   { label: '资源名称', prop: 'name', align: 'center' },
@@ -108,6 +170,9 @@ const handleEditCategory = (row) => {
 }
 const handleDelete = (row) => {
   console.log(row)
+}
+const handleJumpCategory = (row) => {
+  router.push({ name: 'resource-category' })
 }
 </script>
 
