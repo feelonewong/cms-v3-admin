bf69e331eca65b250e94389d8ec137deca23e319
feelonewong <feelonewong@gmail.com>
2023-07-01 22:26:38 +0800

    update: 98上传进度条

--- src/views/course/components/update-course.vue
+++ src/views/course/components/update-course.vue
@@ -46,7 +46,38 @@
           </el-form-item>
         </div>
         <div v-show="currentStep === 1">
-          <h1>课程封面</h1>
+          <el-form-item label="展示图片 ">
+            <el-progress
+              v-show="isShowProgress"
+              type="circle"
+              :percentage="percentage"
+              :width="178"
+            />
+            <el-upload
+              v-show="!isShowProgress"
+              class="avatar-uploader"
+              action="http://39.97.218.60/boss/course/upload"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :on-progress="handleProgress"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageUrl" :src="imageUrl" class="avatar" @load="isShowProgress = false" />
+              <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="展示图片 ">
+            <el-upload
+              class="avatar-uploader"
+              action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload"
+            >
+              <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+              <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+            </el-upload>
+          </el-form-item>
         </div>
         <div v-show="currentStep === 2">
           <h1>销售信息</h1>
@@ -75,9 +106,17 @@
 import { Edit, Picture, UploadFilled } from '@element-plus/icons-vue'
 import { useRouter } from 'vue-router'
 import { reactive, ref } from 'vue'
+import { Plus } from '@element-plus/icons-vue'
+import type { UploadProgressEvent, UploadProps } from 'element-plus'
+import { ElMessage } from 'element-plus/lib/components/index.js'
+
 const props = defineProps({
   courseId: Number
 })
+// 进度条的数值
+const percentage = ref(0)
+// 是否显示进度条
+const isShowProgress = ref(false)
 const router = useRouter()
 const currentStep = ref(0)
 const course = reactive({
@@ -91,13 +130,56 @@ const course = reactive({
   },
   previewFirstField: '',
   previewSecondField: '',
-  sortNum: 0
+  sortNum: 0,
+  courseListImg: '',
+  courseImgUrl: ''
 })
 const goBack = () => {
   router.go(-1)
 }
-</script>
 
+const imageUrl = ref('')
+
+const handleAvatarSuccess: UploadProps['onSuccess'] = (response: any, uploadFile: any) => {
+  imageUrl.value = URL.createObjectURL(uploadFile.raw!)
+  course.courseImgUrl = response.data.name
+}
+
+const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile: any) => {
+  //
+  if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error('Avatar picture size can not exceed 2MB!')
+    return false
+  }
+  isShowProgress.value = true
+  return true
+}
+const handleProgress = (evt: UploadProgressEvent) => {
+  percentage.value = Math.round((evt.loaded / evt.total) * 100)
+}
+</script>
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>
 <style scoped lang="scss">
 .card-header {
   display: flex;
@@ -128,5 +210,11 @@ const goBack = () => {
 }
 .el-form {
   padding: 0 180px;
+}
+
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
