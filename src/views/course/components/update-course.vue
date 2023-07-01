1282e5084b14e3fe137e5b208983a4fcc1b5c55d
feelonewong <feelonewong@gmail.com>
2023-07-01 17:59:45 +0800

    update: 96课程基本信息

--- src/views/course/components/update-course.vue
+++ src/views/course/components/update-course.vue
@@ -18,6 +18,55 @@
           <el-step title="课程详情" :icon="Picture" @click="currentStep = 4" />
         </el-steps>
       </template>
+      <el-form :model="course" label-width="120px" size="large">
+        <div v-show="currentStep === 0">
+          <el-form-item label="课程名称" prop="courseName">
+            <el-input v-model="course.courseName" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="课程简介" prop="brief">
+            <el-input v-model="course.brief" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="讲师姓名" prop="teacherName">
+            <el-input v-model="course.teacherDTO.teacherName" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="讲师职位" prop="position">
+            <el-input v-model="course.teacherDTO.position" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="讲师简介" prop="description">
+            <el-input v-model="course.teacherDTO.description" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="课程概述1" prop="previewFirstField">
+            <el-input v-model="course.previewFirstField" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="课程概述2" prop="previewSecondField">
+            <el-input v-model="course.previewSecondField" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="课程排序" prop="sortNum">
+            <el-input v-model.number="course.sortNum" autocomplete="off" />
+          </el-form-item>
+        </div>
+        <div v-show="currentStep === 1">
+          <h1>课程封面</h1>
+        </div>
+        <div v-show="currentStep === 2">
+          <h1>销售信息</h1>
+        </div>
+        <div v-show="currentStep === 3">
+          <h1>秒杀活动</h1>
+        </div>
+        <div v-show="currentStep === 4">
+          <h1>课程详情</h1>
+        </div>
+        <div class="form-bottom-btn">
+          <el-button type="primary" v-show="currentStep != 0" @click="currentStep--"
+            >上一步</el-button
+          >
+          <el-button type="primary" v-show="currentStep != 4" @click="currentStep++"
+            >下一步</el-button
+          >
+          <el-button type="primary" v-show="currentStep === 4">保存</el-button>
+        </div>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -25,12 +74,25 @@
 <script lang="ts" setup>
 import { Edit, Picture, UploadFilled } from '@element-plus/icons-vue'
 import { useRouter } from 'vue-router'
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 const props = defineProps({
   courseId: Number
 })
 const router = useRouter()
 const currentStep = ref(0)
+const course = reactive({
+  // 基本信息
+  courseName: '',
+  brief: '',
+  teacherDTO: {
+    teacherName: '',
+    position: '',
+    description: ''
+  },
+  previewFirstField: '',
+  previewSecondField: '',
+  sortNum: 0
+})
 const goBack = () => {
   router.go(-1)
 }
@@ -57,5 +119,14 @@ const goBack = () => {
 }
 .el-step {
   cursor: pointer;
+}
+.form-bottom-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+.el-form {
+  padding: 0 180px;
 }
 </style>
